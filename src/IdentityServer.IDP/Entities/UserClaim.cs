cf6856d54098ce7d1ad670bf74f7fce119061dd7
Vikash Chauhan <vikashchauhan51@gmail.com>
2022-12-24 15:05:12 +0530

    Added Entities classes

---
+++ src/IdentityServer.IDP/Entities/UserClaim.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.IDP.Entities;
+
+public class UserClaim
+{
+    public Guid Id { get; set; }
+    public string Type { get; set; } = default!;
+    public string Value { get; set; } = default!;
+    public string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+    public Guid UserId { get; set; }
+    public User User { get; set; } = null!;
+}
+
+public class UserClaimConfiguration : IEntityTypeConfiguration<UserClaim>
+{
+    public void Configure(EntityTypeBuilder<UserClaim> builder)
+    {
+        builder.Property(claim => claim.Id).IsRequired();
+        builder.Property(claim => claim.Type).IsRequired().HasMaxLength(200);
+        builder.Property(claim => claim.Value).IsRequired().HasMaxLength(500);
+        builder.Property(claim => claim.ConcurrencyStamp).IsRequired().HasMaxLength(50).IsConcurrencyToken();
+        builder.Property(claim => claim.UserId).IsRequired();
+        builder.HasKey(claim => claim.Id);
+        builder.Navigation(claim => claim.User);
+        builder.ToTable("UserClaims");
+    }
+}
---
+++ src/IdentityServer.IDP/Entities/UserLogin.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.IDP.Entities;
+
+public class UserLogin
+{
+    public Guid Id { get; set; }
+    public string Provider { get; set; }
+    public string ProviderIdentityKey { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+    public string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+}
+
+public class UserLoginConfiguration : IEntityTypeConfiguration<UserLogin>
+{
+    public void Configure(EntityTypeBuilder<UserLogin> builder)
+    {
+        builder.Property(secret => secret.Id).IsRequired();
+        builder.Property(secret => secret.Provider).IsRequired().HasMaxLength(200);
+        builder.Property(secret => secret.ProviderIdentityKey).IsRequired().HasMaxLength(200);
+        builder.Property(secret => secret.ConcurrencyStamp).IsRequired().HasMaxLength(50).IsConcurrencyToken();
+        builder.Property(secret => secret.UserId).IsRequired();
+        builder.HasKey(secret => secret.Id);
+        builder.Navigation(secret => secret.User);
+        builder.ToTable("UserLogins");
+    }
+}
