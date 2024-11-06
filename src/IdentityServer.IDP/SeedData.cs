ffe6c653bf3c54ea067c06e997549679fb46ef78
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-11-06 21:58:12 +0530

    added db migrations

--- src/IdentityServer.IDP/SeedData.cs
+++ src/IdentityServer.IDP/SeedData.cs
@@ -1,79 +1,47 @@
 ﻿using Duende.IdentityServer.EntityFramework.DbContexts;
 using Duende.IdentityServer.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace IdentityServer.IDP;
 
 public class SeedData
 {
-    public static void EnsureSeedData(WebApplication app)
+    public static void InitializeDatabase(IApplicationBuilder app)
     {
-        using (var scope = app.Services
-            .GetRequiredService<IServiceScopeFactory>().CreateScope())
+        using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope())
         {
-            var context = scope.ServiceProvider
-                .GetService<ConfigurationDbContext>();
-            EnsureSeedData(context);
+            serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+            var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            context.Database.Migrate();
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Config.Clients)
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in Config.IdentityResources)
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.ApiScopes.Any())
+            {
+                foreach (var resource in Config.ApiScopes)
+                {
+                    context.ApiScopes.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
         }
     }
 
-
-    private static void EnsureSeedData(ConfigurationDbContext context)
-    {
-        if (!context.Clients.Any())
-        {
-            Log.Debug("Clients being populated");
-            foreach (var client in Config.Clients.ToList())
-            {
-                context.Clients.Add(client.ToEntity());
-            }
-            context.SaveChanges();
-        }
-        else
-        {
-            Log.Debug("Clients already populated");
-        }
-
-        if (!context.IdentityResources.Any())
-        {
-            Log.Debug("IdentityResources being populated");
-            foreach (var resource in Config.IdentityResources.ToList())
-            {
-                context.IdentityResources.Add(resource.ToEntity());
-            }
-            context.SaveChanges();
-        }
-        else
-        {
-            Log.Debug("IdentityResources already populated");
-        }
-
-        if (!context.ApiScopes.Any())
-        {
-            Log.Debug("ApiScopes being populated");
-            foreach (var resource in Config.ApiScopes.ToList())
-            {
-                context.ApiScopes.Add(resource.ToEntity());
-            }
-            context.SaveChanges();
-        }
-        else
-        {
-            Log.Debug("ApiScopes already populated");
-        }
-
-        if (!context.ApiResources.Any())
-        {
-            Log.Debug("ApiResources being populated");
-            foreach (var resource in Config.ApiResources.ToList())
-            {
-                context.ApiResources.Add(resource.ToEntity());
-            }
-            context.SaveChanges();
-        }
-        else
-        {
-            Log.Debug("ApiResources already populated");
-        }
-    }
 }
